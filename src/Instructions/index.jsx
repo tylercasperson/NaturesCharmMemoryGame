9c621317b59b64642fc0d0b369b25c6f7f10e052
Tyler Casperson <tylercasperson@gmail.com>
2020-03-09 16:49:12 -0600

    wrote in game instructions

---
+++ src/Instructions/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function Instructions() {
+  return (
+    <div className="instruction">
+        <h4>Click on an image to earn points, but don't click on any more than once.</h4>
+    </div>
+  );
+}
+
+export default Instructions;
